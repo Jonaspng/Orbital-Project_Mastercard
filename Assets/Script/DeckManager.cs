b20405b02b3fd2006211d4aa5c4b44a25f83dff7
Jonaspng <jonaspng@hotmail.com>
2022-05-19 16:38:46 +0800

    reversion error corrected

--- Assets/Script/DeckManager.cs
+++ Assets/Script/DeckManager.cs
@@ -1,11 +1,14 @@
 using System.IO;
 using UnityEngine;
+using System.Collections.Generic;
 
 [DefaultExecutionOrder(-1)]
 [System.Serializable]
 public class DeckManager : MonoBehaviour {
 
     public string deckPath;
+
+    public DeckID currentDeckID;
     
     public Deck currentDeck;
 
@@ -15,36 +18,43 @@ public class DeckManager : MonoBehaviour {
 
     public Deck currentHand;
     
-    public GameObject BasicAttackPrefab;
-
-    public GameObject DefendPrefab;
+    public List<GameObject> prefabList;
 
     public GameObject panel;
 
 
 
     void Start() {
-        deckPath = $"{Application.persistentDataPath}/deck.json";
+        deckPath = $"{Application.persistentDataPath}/deckID.json";
         if (File.Exists(deckPath)) {
             string json = File.ReadAllText(deckPath);
-            currentDeck = JsonUtility.FromJson<Deck>(json);
+            currentDeckID = JsonUtility.FromJson<DeckID>(json);
+            DeckIdToDeck();
         } else {
             currentDeck = new Deck();
-            currentDeck.AddCard(Instantiate(BasicAttackPrefab));
-            currentDeck.AddCard(Instantiate(BasicAttackPrefab));
-            currentDeck.AddCard(Instantiate(BasicAttackPrefab));
-            currentDeck.AddCard(Instantiate(BasicAttackPrefab));
-            currentDeck.AddCard(Instantiate(BasicAttackPrefab));
-            currentDeck.AddCard(Instantiate(DefendPrefab));
-            currentDeck.AddCard(Instantiate(DefendPrefab));
-            currentDeck.AddCard(Instantiate(DefendPrefab));
-            currentDeck.AddCard(Instantiate(DefendPrefab));
-            currentDeck.AddCard(Instantiate(DefendPrefab));
-            SaveJson(currentDeck);
+            currentDeckID = new DeckID(); 
+            currentDeckID.AddCardID(0);
+            currentDeckID.AddCardID(0);
+            currentDeckID.AddCardID(0);
+            currentDeckID.AddCardID(0);
+            currentDeckID.AddCardID(0);
+            currentDeckID.AddCardID(1);
+            currentDeckID.AddCardID(1);
+            currentDeckID.AddCardID(1);
+            currentDeckID.AddCardID(1);
+            currentDeckID.AddCardID(1);
+            SaveJson(currentDeckID);
+            DeckIdToDeck();
         }
     }
 
-    public void SaveJson(Deck newDeck) {
+     public void DeckIdToDeck() {
+        foreach (int id in currentDeckID.cardIDList) {
+            currentDeck.AddCard(Instantiate(prefabList[id]));            
+        }
+    }
+
+    public void SaveJson(DeckID newDeck) {
         string json = JsonUtility.ToJson(newDeck);
         File.WriteAllText(deckPath, json);
     }
@@ -52,7 +62,7 @@ public class DeckManager : MonoBehaviour {
     public void Initialise() {
         unusedPile = currentDeck;
         Deck.Shuffle(unusedPile);
-        //DrawCard(5);
+        DrawCard(5);
     }
 
     // public void Reshuffle(int numOfShuffles) {
@@ -68,22 +78,22 @@ public class DeckManager : MonoBehaviour {
         int numOfCardLeft = unusedPile.cardList.Count;
         if (numOfCardLeft < numOfCards) {
            print("test");
-            for (int i = 0; i < numOfCardLeft; i++) {
+            for (int i = 0; i < numOfCards; i++) {
                 currentHand.AddCard(unusedPile.cardList[i]);
-                unusedPile.cardList[i].transform.SetParent(panel.transform);
+                currentHand.cardList[i].transform.SetParent(panel.transform);
                 unusedPile.RemoveCard(unusedPile.cardList[i]);
                 usedPile.AddCard(unusedPile.cardList[i]);
             }
-            // this.Reshuffle(numOfCardLeft);       
+            //this.Reshuffle(numOfCardLeft);       
             // usedPile.CopyTo(unusedPile);
             // usedPile.ResetDeck();
             // Deck.Shuffle(unusedPile);
             // this.Reshuffle(numOfCards - numOfCardLeft);
         } else {
              print("test");
-            for (int i = 0; i < numOfCardLeft; i++) {
+            for (int i = 0; i < numOfCards; i++) {
                 currentHand.AddCard(unusedPile.cardList[i]);
-                unusedPile.cardList[i].transform.SetParent(panel.transform);
+                currentHand.cardList[i].transform.SetParent(panel.transform);
                 unusedPile.RemoveCard(unusedPile.cardList[i]);
                 usedPile.AddCard(unusedPile.cardList[i]);
             }
---
+++ Assets/Script/TurnEvent/DeckID.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DeckID {
+
+    public List<int> cardIDList;
+
+    public DeckID() {
+        this.cardIDList = new List<int>(); 
+    }
+
+    public void AddCardID(int cardID) {
+        this.cardIDList.Add(cardID);
+    }
+
+    public void RemoveCard(int cardID) {
+        this.cardIDList.Remove(cardID);
+    }
+
+    public void CopyTo(DeckID deck) {
+        List<int> newList = new List<int>(this.cardIDList);
+        deck.cardIDList = newList;
+    }
+
+
+
+
+}

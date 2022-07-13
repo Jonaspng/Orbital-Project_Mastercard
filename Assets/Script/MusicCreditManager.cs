d1ce34c3777bae78dec3aaf220f88b3b04297982
Jonaspng <jonaspng@hotmail.com>
2022-07-13 16:08:38 +0800

    Updated Credits scene

---
+++ Assets/Script/MusicCreditManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicCreditManager : MonoBehaviour {
+
+    public void OnStartMenuClick() {
+        Application.OpenURL("https://freesound.org/people/ShadyDave/sounds/262259/");
+    }
+    
+    public void OnStageMusicClick() {
+        Application.OpenURL("https://freesound.org/people/Migfus20/sounds/615752/");
+    }
+
+    public void OnEventMusicClick() {
+        Application.OpenURL("https://freesound.org/people/CyberMad/sounds/448060/");
+    }
+
+    public void OnTypingClick() {
+        Application.OpenURL("https://www.youtube.com/watch?v=2BUNHd7ENZk");
+    }
+
+    public void OnSwordEffectClick() {
+        Application.OpenURL("https://freesound.org/people/qubodup/sounds/184422/");
+    }
+
+    public void OnArrowEffectClick() {
+        Application.OpenURL("https://freesound.org/people/krishnamohan/sounds/486971/");
+        
+    }
+
+    public void OnShieldEffectClick() {
+        Application.OpenURL("https://freesound.org/people/StormwaveAudio/sounds/614074/");
+    }
+
+    public void OnMagicEffectClick() {
+        Application.OpenURL("https://freesound.org/people/qubodup/sounds/442872/");
+        
+    }
+
+    
+
+    
+}
